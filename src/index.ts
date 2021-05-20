ce02f45e79460ef24551f0d7190b5b54260a4a75
ehayter-ts <ernie.hayter@k2.com>
2021-05-20 13:57:08 +0100

    Fix JSON

--- src/index.ts
+++ src/index.ts
@@ -1050,7 +1050,7 @@ function CreatePlanTask(parameters: SingleRecord, properties: SingleRecord, cb) 
 
     var url = baseUriEndpoint + '/planner/tasks';
 
-    var data = `"{\"planId\":\"${planId}\",\"title\":\"${taskTitle}\",\"dueDateTime\":\"${taskDueDate}\",\"bucketId\":\"${bucketId}\",\"assignments\":{\"${taskUserId}\":{\"@odata.type\":\"#microsoft.graph.plannerAssignment\",\"orderHint\":\" !\"}}}"`;
+    var data = `{\"planId\":\"${planId}\",\"title\":\"${taskTitle}\",\"dueDateTime\":\"${taskDueDate}\",\"bucketId\":\"${bucketId}\",\"assignments\":{\"${taskUserId}\":{\"@odata.type\":\"#microsoft.graph.plannerAssignment\",\"orderHint\":\" !\"}}}`;
 
     ExecuteRequest(url, data, "POST", function (responseText) {
         if (typeof cb === 'function')
